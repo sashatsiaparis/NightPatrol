fa23677eeecb8e607a4d7f0bb21f065da8e1f3fc
Sasha <sashatsiaparis@gmail.com>
2019-10-24 22:13:48 +1100

    transitions work with contacts and split for user type

--- app/src/main/java/com/example/nightpatrol/LandingScreen.java
+++ app/src/main/java/com/example/nightpatrol/LandingScreen.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.nightpatrol.api.model.CurrentUser;
@@ -54,6 +55,14 @@ public class LandingScreen extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
 
         mTOKEN = getIntent().getStringExtra("token");
+        ImageView settingsImage = findViewById(R.id.imageSettings);
+        settingsImage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intentSettings = new Intent(LandingScreen.this, Settings.class);
+                intentSettings.putExtra("token", mTOKEN);
+                startActivity(intentSettings);
+            }
+        });
 
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
